def03d2e44ee6759c6bb56b1e9039e7dd7015777
brcthn <burcu1818@gmail.com>
2020-12-25 23:20:54 +0000

    Version-2 with radio  button

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
-const data={
-    USD:{EUR: 0.82, GBP: 0.74, AUD:1.32},
-    EUR:{USD: 1.23, GBP: 0.91, AUD:1.61},
+const data = {
+    USD: {EUR: 0.82, GBP: 0.74,AUD:1.32},
+    EUR: {USD: 1.23, GBP: 0.91,AUD:1.61},
     GBP: {USD: 1.35, EUR: 1.10,AUD:1.77},
     AUD:{EUR:0.62, USD:0.75, GBP:0.56}
-}
-const currencyKeys = Object.keys(data);
-// root= currencybox-from or currencybox-to  
-function createCurrencyElements(currencyKeys,root,name){
-    for(let i=0;i<currencyKeys.length;i++){
-        const optionElement=document.createElement("option");
-        optionElement.setAttribute("value",currencyKeys[i]);
-        optionElement.setAttribute("id",root);
-        optionElement.innerHTML= `${currencyKeys[i]}`;
-        const selectElement=document.getElementById(name)
-        selectElement.append(optionElement)
+};
+
+ const currencyKeys = Object.keys(data);
+
+ function createCurrencyElements(elements,inputName,root){
+     console.log(root)
+    for(let i =0; i< elements.length; i++){
+        const inputElement=document.createElement("input");
+        inputElement.setAttribute("type","radio");
+        inputElement.setAttribute("name",inputName);
+        inputElement.setAttribute("value",elements[i]);
+        inputElement.setAttribute("id", inputName+elements[i]);
+
+        const labelElement=document.createElement("label");
+        labelElement.textContent=elements[i];
+        labelElement.setAttribute("for", inputName+elements[i]);
+       
+        const currencyDiv=document.getElementById(root);
+        currencyDiv.appendChild(inputElement);
+        currencyDiv.appendChild(labelElement);
     }
-}
-createCurrencyElements(currencyKeys,"currencybox-from","currency-dropdown-from");
-createCurrencyElements(currencyKeys,"currencybox-to","currency-dropdown-to")
+ }
+ const currencyFrom = document.querySelector("#currency-from");
+ createCurrencyElements(currencyKeys, "currency-from","currency-from")
 
-const calculateButton=document.getElementById("calculate-button");
-calculateButton.addEventListener("click",result);
+ const currencyTo=document.querySelector("#currency-to")
+ createCurrencyElements(currencyKeys,"currency-to","currency-to")
 
-function result(){
-    //for get select value (from)
-    const getFromSelect=document.getElementById("currency-dropdown-from");
-    const fromTarget=getFromSelect.value;
-    //for get select value(to)
-    const getToSelect=document.getElementById("currency-dropdown-to");
-    const toTarget=getToSelect.value;
-    //result place
-    const currencyResult=document.getElementById("currency-result");
-    if(toTarget == "select"||fromTarget == "select" ){
+ const calculateButton = document.querySelector("#calculate-button");
+ calculateButton.addEventListener("click",result);
+
+ function result(){
+    const fromTarget=document.querySelector("input[name='currency-from']:checked");
+    const toTarget = document.querySelector("input[name='currency-to']:checked");
+    const currencyResult = document.querySelector("#currency-result");
+    if(fromTarget == null || toTarget == null){
         currencyResult.innerHTML="Please Select Currency";
         return;
     }
-    if(toTarget==fromTarget){
+    const amount=document.querySelector("input[name='amount']").value;      
+    if(toTarget.value==fromTarget.value){
         currencyResult.innerHTML= "Please Select Different Currency";
         return;
     }   
-    // get amount
-    const amount = document.querySelector("input[name='amount']").value;
     if(!isNaN(Number(amount)) && amount!=0){
-        const getFromCurrency=data[fromTarget];
-        const valueOneCurrency= getFromCurrency[toTarget];
-        const result=amount *valueOneCurrency;
-        oldResult=result;
-        currencyResult.innerHTML= amount+ " "+ fromTarget + " = "+ result.toFixed(4) + " "+ toTarget;
-    }
-    else{
+        const  getObject = data[fromTarget.value];
+        const resultForOne=getObject[toTarget.value];
+        const result = amount * resultForOne;
+        currencyResult.innerHTML = amount + " " + fromTarget.value + " = " + result.toFixed(4) + " " + toTarget.value;
+    }else{
         currencyResult.innerHTML="Please Enter Number";
-    }
+    } 
 }
